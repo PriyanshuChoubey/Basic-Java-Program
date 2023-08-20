8411117ebd1373e2771c0cf635cbc0853a1195f5
Priyanshu Kumar choubey <104070944+PriyanshuChoubey@users.noreply.github.com>
2023-08-20 19:12:20 +0530

    Final code

--- NumberGame.java
+++ NumberGame.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class NumberGame {
-    public int min=1;
-    public int max=10;
-    int level=0;
+
+    int level=1;
     int score=0;
     int flag=0;
     public void startGame(int realNum)
     {
         Scanner take = new Scanner(System.in);
-        System.out.println("NOTE: You have only 3 attempts!");
-        int attempt=3;
+        int attempt=5;
+
+        System.out.print("|| LEVEL: "+level+" || SCORE: "+score+" ");
         while(attempt>0)
         {
-            System.out.println("Guess the number between "+min+" and "+max);
-
+            System.out.print("|| ATTEMPTS LEFT: "+attempt+" ||");
+            System.out.println();
             int guessNum= take.nextInt();
             if(guessNum==realNum)
             {
                 System.out.println("Congratulations!! You guessed correctly.");
-                score++;
+                score+=10;
                 level++;
                 flag=1;
                 break;
@@ -30,7 +30,8 @@ public class NumberGame {
                 System.out.println("Too Low!");
             }
             else {
-                System.out.println("Wrong guess!");
+                if(attempt!=1)
+                    System.out.println("Try again you are close!");
             }
             attempt--;
             flag=0;
@@ -40,16 +41,21 @@ public class NumberGame {
     }
 
     public static void main(String[] args) {
+        int min=1;
+        int max=10;
         Scanner take = new Scanner(System.in);
         NumberGame ng = new NumberGame();
-        int realNum=(int)(Math.random()*(ng.max-ng.min+1)+ng.min);
+        int realNum=(int)(Math.random()*(max-min+1)+min);
+        System.out.println("On each level completion 10 points will be rewarded.");
+        System.out.println("Guess the number between "+min+" and "+max);
         ng.startGame(realNum);
         while (ng.flag==1) {
-            ng.startGame((int) (Math.random() * (ng.max - ng.min + 1) + ng.min));
+            ng.startGame((int) (Math.random() * (max - min + 1) + min));
         }
-        System.out.println("GAME OVER!");
-        System.out.println("Score="+ng.score);
-        System.out.println("Level="+ng.level);
+        System.out.println();
+        System.out.println("|| Score="+ng.score+" || Level Completed:"+(ng.level-1)+" ||");
         System.out.println("THANK YOU :)");
+        System.out.println();
+        System.out.println("** GAME OVER **");
     }
 }
